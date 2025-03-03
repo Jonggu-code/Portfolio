958b621be48f9b23804ddb93dd154422896ac5d8
Jonggu-code <00whdcks@gmail.com>
2025-03-03 18:41:27 +0900

    0303 Update
    맛맵 modal 수정

--- src/modals/ModalMatmap.jsx
+++ src/modals/ModalMatmap.jsx
@@ -216,32 +216,44 @@ const ModalMatmap = ({ toggleModal }) => {
               </div>
               <ul className="modalToggleList">
                 <li>
-                  이력서와 비슷한 목차와 내용으로 구성해 자기소개와 프로젝트
-                  설명을 한눈에 볼 수 있도록 제작했습니다.
+                  약 3주간의 기간동안 교육원이 위치하고 있는 역삼역 주변의 맛집
+                  검색 사이트를 카카오맵 API 를 활용해 제작하였습니다.
                 </li>
-                <li>길어지는 내용은 토글창으로 구성해 가독성을 높혔습니다.</li>
+                <li>
+                  각 음식점 위치에 마커가 뜨며 클릭하면 직접 제작한 커스텀
+                  인포윈도우가 뜨며 음식점 정보를 간결하게 확인할 수 있습니다.
+                </li>
+                <li>
+                  우측 상단에 위치한 유저박스 왼쪽에 있는 아이콘을 클릭하면 현재
+                  내가 보고있는 화면을 기준으로 음식점을 재검색 할 수 있습니다.
+                </li>
               </ul>
               <div className="modalToggle">
                 <span>▶</span> <b>회원 중심적 서비스 구성</b>
               </div>
               <ul className="modalToggleList">
-                <li>메인컬러를 설정하고 사이트 전체적으로 활용했습니다.</li>
                 <li>
-                  차트를 내릴수록 어두워지는 배경으로 시각적으로 편안할 수
-                  있도록 디자인했습니다.
+                  각 음식점은 유저가 남긴 리뷰 갯수, 찜 갯수를 볼 수 있으며 이
+                  수치는 '월간 맛집 랭킹' 을 선정하는데에 사용됩니다.
+                </li>
+                <li>
+                  로그인을 한 유저는 내가 찜한 식당, 내가 남긴 리뷰 들을
+                  마이페이지에서 확인할 수 있고, 각 컨텐츠를 모아서 볼 수 있는
+                  페이지가 있습니다.
                 </li>
               </ul>
               <div className="modalToggle">
-                <span>▶</span> <b>평점에 따른 '이달의 맛집 랭킹' 서비스</b>
+                <span>▶</span> <b>평점에 따른 '월간 맛집 랭킹' 서비스</b>
               </div>
               <ul className="modalToggleList">
                 <li>
-                  네비게이션 바에서 다크모드 / 라이트모드 를 사용자의 편의에
-                  따라 조정할 수 있습니다.
+                  매달 가장 많은 찜을 받은 음식점 중에서 리뷰 평균 평점이 높은
+                  순으로 '월간 맛집 랭킹'을 선정합니다.
                 </li>
                 <li>
-                  컴포넌트 전역적으로 다크모드 / 라이트모드를 구분하기 위해{" "}
-                  <code>DarkModeContext</code> 를 생성해 활용했습니다.
+                  랭킹 페이지에서는 각 음식점의 평점, 찜 갯수, 리뷰 갯수, 주소,
+                  영업 유무 등의 음식점 정보를 확인할 수 있으며, 음식점을
+                  클릭하면 상세 페이지로 이동합니다.
                 </li>
               </ul>
               <div className="modalToggle">
@@ -253,13 +265,13 @@ const ModalMatmap = ({ toggleModal }) => {
               </div>
               <ul className="modalToggleList">
                 <li>
-                  스크롤 이벤트에 따라 각 컨텐츠가 나타나는 애니메이션을
-                  구현했습니다.
+                  음식점 DB에 저장되어있는 일자별 영업시간과 현재 시간을 비교해
+                  영업 유무를 표시합니다.
                 </li>
                 <li>
-                  <code>UseRef</code>를 활용해 각 컨텐츠의 위치를 파악하고,{" "}
-                  <code>useScrollSection</code>을 통해 현재 스크롤 위치에서
-                  벗어나면 컨텐츠들이 fadeOut 되도록 구성했습니다.
+                  음식점 정보를 볼 때 현재 갈 수 있는 식당인지, 브레이크 타임은
+                  아닌지와 같은 실시간 영업 정보를 한눈에 볼 수 있으면 좋을 것
+                  같았습니다.
                 </li>
               </ul>
             </div>
@@ -270,9 +282,14 @@ const ModalMatmap = ({ toggleModal }) => {
               </div>
               <ul className="modalToggleList">
                 <li>
-                  현재 PC 환경에 최적화된 디자인으로 제작되었으나, 모바일
-                  환경에서도 최적화된 디자인을 제공할 수 있도록 반응형 웹
-                  디자인을 추가할 예정입니다.
+                  전체 프로젝트의 기획을 담당하였으며, 프로젝트의 주제와 목표를
+                  설정하고, 메인컬러 선정과 로고를 제작하고 각 중요 페이지들을
+                  제작하였습니다.
+                </li>
+                <li>
+                  최우선 목표로 <b>가독성</b>을 생각하였고, 다음으로{" "}
+                  <b>유저 편의성과 만족스러운 경험</b>을 중점으로 디자인을
+                  진행했습니다.
                 </li>
               </ul>
               <div className="modalToggle">
@@ -281,26 +298,44 @@ const ModalMatmap = ({ toggleModal }) => {
               </div>
               <ul className="modalToggleList">
                 <li>
-                  현재는 각 프로젝트가 큰 박스 형태로 제공하고 있으나,
-                  프로젝트가 추가되면 화면이 길어지는 단점이 있습니다.
+                  팀원들의 역할 분담과 일정을 관리하였으며, 팀원들 간의
+                  커뮤니케이션을 원활하게 하기 위해 매일 저녁 스크럼을
+                  진행하였습니다.
                 </li>
                 <li>
-                  전체적인 레이아웃을 변경해 각 프로젝트들을 더 간략한 박스
-                  형태로 변경하고, detail 을 통해 내용을 파악할 수 있도록 변경할
-                  예정입니다.
+                  결과적으로, 팀원들 간의 의견 충돌을 최소화하고 작업 일정을
+                  부담없이 소화하며 프로젝트를 원활하게 진행할 수 있었습니다.
                 </li>
               </ul>
               <div className="modalToggle">
                 <span>▶</span>{" "}
                 <b>
-                  페이지 제작 (메인화면, 맛집 상세 페이지, 리뷰 페이지, 랭킹
-                  페이지, 마이페이지, 후기 작성 페이지)
+                  페이지 제작 (메인화면, 음식점 상세 페이지, 후기 작성 페이지,
+                  월간 맛집 랭킹 페이지, 마이페이지 )
                 </b>
               </div>
               <ul className="modalToggleList">
                 <li>
-                  피드백을 통해 교육 수료 현황과 경력 사항이 있어야 한다는
-                  사실을 깨닫고, 추가할 예정입니다.
+                  <b>메인화면</b> : 카카오맵 API 를 활용해 JS로 각 음식점 마커
+                  배치와 커스텀 인포윈도우 제작 및 유저박스 구현
+                </li>
+                <li>
+                  <b>페이지 공통 헤더 제작</b> : 메인 화면을 제외한 모든
+                  페이지의 공통 헤더 제작
+                </li>
+                <li>
+                  <b>음식점 상세 페이지</b> : 화면 상단 슬라이더와 음식점 정보,
+                  리뷰 작성, 찜하기 버튼 등의 기능을 구현. 스크롤 따라다니는
+                  추천 맛집 박스 구현
+                </li>
+                <li>
+                  <b>후기 작성 페이지</b> : 각 항목 별점입력 기능, AJAX 활용
+                  다중 이미지 업로드 기능 구현, 이미지 업로드 시 파일 형식, 크기
+                  유효성 체크
+                </li>
+                <li>
+                  <b>마이페이지</b> : 내가 찜한 식당, 내가 남긴 리뷰 확인 기능
+                  구현 및 개인 정보 수정 기능 구현
                 </li>
               </ul>
               <div className="modalToggle">
@@ -309,8 +344,9 @@ const ModalMatmap = ({ toggleModal }) => {
               </div>
               <ul className="modalToggleList">
                 <li>
-                  피드백을 통해 교육 수료 현황과 경력 사항이 있어야 한다는
-                  사실을 깨닫고, 추가할 예정입니다.
+                  프로젝트의 디자인 스타일과 레이아웃을 통일하기 위해 프론트
+                  팀원들과 논의하고, 통일 된 디자인 스타일을 적용,
+                  수정하였습니다.
                 </li>
               </ul>
             </div>
@@ -325,21 +361,15 @@ const ModalMatmap = ({ toggleModal }) => {
               </div>
               <ul className="modalToggleList">
                 <li className="text-red-600">
-                  Notion API를 활용해 프로젝트 상세 내용을 모달창으로 불러오려
-                  했으나, 불러오는 과정에서{" "}
-                  <b>버전 충돌과 Notion API Key 오류</b>로 인해 프로젝트 상세
-                  내용을 원할하게 불러오지 못하는 문제가 발생했습니다.
+                  이미지를 추가할 때 마다 이미 업로드해 놓은 이미지들이
+                  리셋되고, 업로드한 이미지를 삭제하는 버튼이 제대로 동작하지
+                  않는 오류가 있었습니다.
                 </li>
                 <li className="text-customBlue">
-                  이를 해결하기 위해 Notion API를 활용하지 않고,{" "}
-                  <b>직접 프로젝트 상세 내용을 전달할 수 있는 모달창을 제작</b>
-                  했습니다.
-                </li>
-                <li className="text-customBlue">
-                  포트폴리오 공유 사이트{" "}
-                  <b>Behance 의 레이아웃을 참고해서 디자인</b>
-                  했으며, 사이드 버튼 구성은 프로젝트 모달창의 목적에 맞게
-                  재구성 했습니다.
+                  이를 해결하기 위해 업로드된 각 이미지 파일에
+                  <code>data-id : uniqueId</code>로 고유한 id를 부여한 뒤
+                  이미지를 추가할 때마다 해당 객체를 배열에 추가하고, 삭제할 때
+                  해당 객체를 배열에서 제거하는 방식으로 해결하였습니다.
                 </li>
               </ul>
               <div className="modalToggle">
@@ -350,25 +380,17 @@ const ModalMatmap = ({ toggleModal }) => {
               </div>
               <ul className="modalToggleList">
                 <li className="text-red-600">
-                  <b>
-                    프로젝트 모달창에서 각 Toggle 항목을 클릭했을 때 제대로
-                    동작하지 않는 오류
-                  </b>
-                  가 있었고, 렌더링 시 해당 기능을 하는 <code>UseEffect</code>{" "}
-                  가 제대로 동작하지 않는 문제를 발견하였습니다.
+                  식당 상세 페이지에서 후기 사진과 후기 갯수가 많아질 때
+                  스크롤이 길어져 사용자 경험이 좋지 않았습니다.
                 </li>
                 <li className="text-customBlue">
-                  <b>
-                    각 기능을 <code>handleToggleClick</code>,{" "}
-                    <code>handleImageClick</code> 함수로 분리
-                  </b>
-                  하여 이벤트 리스너를 추가할 때 동일한 함수를 사용하도록
-                  수정했습니다.
+                  <b>더보기 버튼</b>을 만들어 사용자가 원할 때 더 많은 사진과
+                  후기를 볼 수 있도록 하였습니다.
                 </li>
                 <li className="text-customBlue">
-                  또한, 언마운트 시 이벤트 리스너를 제거하여{" "}
-                  <b>불필요한 메모리 누수를 방지하고 성능을 최적화</b>
-                  하였습니다.
+                  또한, 현재 화면에 보여지는 사진과 후기 갯수를 카운팅하여 끝에
+                  다다랐을 때 더보기 버튼을 숨기도록 하여 사용자 경험을
+                  개선하였습니다.
                 </li>
               </ul>
               <div className="modalToggle">
@@ -376,25 +398,17 @@ const ModalMatmap = ({ toggleModal }) => {
               </div>
               <ul className="modalToggleList">
                 <li className="text-red-600">
-                  <b>
-                    프로젝트 모달창에서 각 Toggle 항목을 클릭했을 때 제대로
-                    동작하지 않는 오류
-                  </b>
-                  가 있었고, 렌더링 시 해당 기능을 하는 <code>UseEffect</code>{" "}
-                  가 제대로 동작하지 않는 문제를 발견하였습니다.
+                  검색기록이 없을 때 빈화면이 뜨는것이 사용자 경험에 좋지
+                  않았습니다.
                 </li>
                 <li className="text-customBlue">
-                  <b>
-                    각 기능을 <code>handleToggleClick</code>,{" "}
-                    <code>handleImageClick</code> 함수로 분리
-                  </b>
-                  하여 이벤트 리스너를 추가할 때 동일한 함수를 사용하도록
-                  수정했습니다.
+                  메인화면에서 검색기록이 뜨지 않을 때의 기본 화면을
+                  추가하였습니다.
                 </li>
                 <li className="text-customBlue">
-                  또한, 언마운트 시 이벤트 리스너를 제거하여{" "}
-                  <b>불필요한 메모리 누수를 방지하고 성능을 최적화</b>
-                  하였습니다.
+                  또한, 처음 회원가입한 유저가 마이페이지를 갔을 때 내가 찜한
+                  맛집과 내가 남긴 리뷰가 없을 때 보이는 기본 화면과 각 페이지
+                  기본 화면도 같이 추가하였습니다.
                 </li>
               </ul>
             </div>
@@ -407,28 +421,36 @@ const ModalMatmap = ({ toggleModal }) => {
               </h1>
               <ul className="modalImgList">
                 <li>
-                  <img src="./portfolio/page1.jpg" className="h-auto" />
-                  <span>자기 소개</span>
+                  <img src="./Matmap/modal/page1.png" className="h-auto" />
+                  <span>메인 화면</span>
                 </li>
                 <li>
-                  <img src="./portfolio/page2.jpg" className="h-auto" />
-                  <span>About Me</span>
+                  <img src="./Matmap/modal/page2.jpg" className="h-auto" />
+                  <span>음식점 상세 페이지</span>
                 </li>
                 <li>
-                  <img src="./portfolio/page3.jpg" className="h-auto" />
-                  <span>Skills</span>
+                  <img src="./Matmap/modal/page3.png" className="h-auto" />
+                  <span>맛집 랭킹 페이지</span>
                 </li>
                 <li>
-                  <img src="./portfolio/page4.jpg" className="h-auto" />
-                  <span>Project</span>
+                  <img src="./Matmap/modal/page4.png" className="h-auto" />
+                  <span>후기 작성 1페이지</span>
                 </li>
                 <li>
-                  <img src="./portfolio/page5.jpg" className="h-auto" />
-                  <span>Project Detail</span>
+                  <img src="./Matmap/modal/page5.png" className="h-auto" />
+                  <span>후기 작성 2페이지</span>
                 </li>
                 <li>
-                  <img src="./portfolio/page6.jpg" className="h-auto" />
-                  <span>Contact</span>
+                  <img src="./Matmap/modal/page6.png" className="h-auto" />
+                  <span>마이페이지</span>
+                </li>
+                <li>
+                  <img src="./Matmap/modal/page7.png" className="h-auto" />
+                  <span>로그인</span>
+                </li>
+                <li>
+                  <img src="./Matmap/modal/page8.png" className="h-auto" />
+                  <span>회원가입</span>
                 </li>
               </ul>
             </div>
